721d6d2eee9f1e4581fefbe616fe9a3b229ce922
Your Name <charlesmartins.ads@gmail.com>
2021-02-25 10:40:35 -0300

    dynamically changes the user level

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,9 +1,15 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 const avatarUrl = "https://avatars.githubusercontent.com/u/60299718?s=400&u=435f6098dd446f0426b498c26a1bfe335c83abf9&v=4"
 let userAvatar = avatarUrl ? avatarUrl : "/assets/userAvatar.svg";  ;
 
 export function Profile() {
+
+    const {level} = useContext(ChallengesContext)
+
+
     return (
         <div className={ styles.profileContainer }>
             <img src={userAvatar} alt="Charles Martins"/>
@@ -12,7 +18,7 @@ export function Profile() {
                 <strong>Charles Martins</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
                 
             </div>
