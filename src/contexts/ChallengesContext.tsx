da23335e9c77599fdd7cf52b92fb414b1292ea91
Your Name <charlesmartins.ads@gmail.com>
2021-02-25 13:53:49 -0300

    teste sound notification

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -59,17 +59,16 @@ export function ChallengesProvider( {children}:ChallengesProviderProps ) {
 
       setActiveChallenge(challenge)
 
-      new Audio( '/notification.mp3').play();
+     /* new Audio( '/notification.mp3').play();
 
       if ( Notification.permission === 'granted' ) {
          new Notification( 'Novo desafio 🎉 ' ) , {
             body: `Valendo ${challenge.amount}xp!`
          }
    
-      }
+      }*/
    }
-   
-  
+    
 
    /* reset challenge if user failed */
    function resetChallenge() {
