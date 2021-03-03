a68fc1ccf546b54c47a6b7d246f5340a4adddb1b
Your Name <charlesmartins.ads@gmail.com>
2021-03-03 17:11:24 -0300

    removed the result of failed and total challenges

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,27 +1,21 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+
+//import { CreatesChartChallenge } from '../components/ChartChallenge';
+
 import styles from '../styles/components/CompletedChallenges.module.css';
 
+
 export function CompletedChallenges() {
-    const { challengesCompleted, challengesFailed, totalChallenge } = useContext(ChallengesContext); 
-    return(
-        <div className={ styles.challengesContainer } >
-            
+    const { challengesCompleted } = useContext(ChallengesContext); 
+    return( 
+       
+        <div className={ styles.challengesContainer } >       
+        
             <div className={ styles.completedChallengesContainer } >                            
                 <span>Desafios completos </span>
                 <span>{challengesCompleted}</span>                   
-            </div>
-        
-            <div className={ styles.FailedchallengesContainer } > 
-                <span> Desafios incompletos </span>
-                <span>{ challengesFailed }</span>                
-            </div>
-
-            <div className={ styles.TotalchallengesContainer } >                
-                <span> Total de desafios </span>
-                <span>{ totalChallenge }</span>   
-   
-            </div>
+            </div>        
 
         </div>
     ); 
