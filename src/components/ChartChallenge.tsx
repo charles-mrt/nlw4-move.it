15689afa044aa9bad9ebd4bd75202e637bdaf4c4
Your Name <charlesmartins.ads@gmail.com>
2021-03-08 18:05:02 -0300

    created a challenge record chart

--- src/components/ChartChallenge.tsx
+++ src/components/ChartChallenge.tsx
@@ -1,6 +1,9 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { Sidebar} from "./Sidebar";
+
 import styles from "../styles/components/ChartChallenge.module.css";
+
 import {
     FiBarChart2,
     FiBarChart,
@@ -8,62 +11,93 @@ import {
     FiUserCheck,
     FiUserMinus,
     FiTrendingDown,
-    FiTrendingUp
+    FiTrendingUp,
+    FiUser,
+    FiArrowUp
 } from 'react-icons/fi';
+import { MainLogo } from './MainLogo';
 
 export function CreatesChartChallenge() {
 
     const { challengesCompleted, challengesFailed, totalChallenge } = useContext(ChallengesContext);
 
     return (
+        <>
+        <Sidebar />
         
-            <div className={styles.chartChallenge}>
-                <details><summary>Dados do desafio - Saiba mais</summary>
-                <section>
-                    <header>Dados dos desafios <FiBarChart2 /></header>
+            <div className={styles.content}><MainLogo />
+                <div className={styles.chartChallenge}>
+                
+                    <section>
+                        <header>                        
+                            <span>Dados dos desafios <FiBarChart2 /></span>
+                            <span> Charles Martins <FiUser /></span>
+                        </header>
+                        <main>
+                            <div className={styles.totalChallenge}>
+                                <strong>total : {totalChallenge} <FiUserCheck /></strong>
 
-                    <div className={styles.totalChallenge}>
-                        <strong>total : {totalChallenge} <FiUserCheck /></strong>
+                                <span style={{ width: `${totalChallenge >= 40 ? 100 : totalChallenge}%` }} />
+                            </div>
 
-                        <span style={{ width: `${totalChallenge >= 40 ? 100 : totalChallenge}%` }} />
-                    </div>
+                            <div className={styles.challengesCompleted}>
+                                <strong>completos: {challengesCompleted} <FiUserPlus /></strong>
+                                <span style={{ width: `${challengesCompleted >= 100 ? 100 : challengesCompleted}%` }} />
+                            </div>
 
-                    <div className={styles.challengesCompleted}>
-                        <strong>completos: {challengesCompleted} <FiUserPlus /></strong>
-                        <span style={{ width: `${challengesCompleted >= 100 ? 100 : challengesCompleted}%` }} />
-                    </div>
+                            <div className={styles.challengesFailed}>
+                                <strong>incompletos: {challengesFailed} <FiUserMinus /></strong>
+                                <span style={{ width: `${challengesFailed >= 100 ? 100 : challengesFailed}%` }} />
+                            </div>
+                        </main>
+                        <div className={styles.challengesReport}>
 
-                    <div className={styles.challengesFailed}>
-                        <strong>incompletos: {challengesFailed} <FiUserMinus /></strong>
-                        <span style={{ width: `${challengesFailed >= 100 ? 100 : challengesFailed}%` }} />
-                    </div>
+                            {totalChallenge === 0 ? (
+                                <h5>
+                                    <FiBarChart color={"#5965e0"} />
+                                    Relatório: Nenhum resultado
+                                </h5>
 
-                    <div className={styles.challengesReport}>
+                            ) : (
+                                    challengesCompleted > challengesFailed ? (
+                                        <h5>
+                                            <FiTrendingUp color={"#5965e0"} />
+                                            Relatório: Seus resultados estão no padrão esperado
+                                        </h5>
+                                    ) :
+                                        <h5>
+                                            <FiTrendingDown color={"#e03a86"} />
+                                            Relatório: Seus resultados estão abaixo do esperado
+                                        </h5>
+                                )
+                            }
+                        
+                            <ul>
+                                <li className={styles.total}> 
+                                    <h4><FiUserCheck /> Total</h4>
+                                    <h4>2</h4>
+                                </li>
 
-                        {totalChallenge === 0 ? (
-                            <h5>
-                                <FiBarChart color={"#5965e0"} />
-                                 Relatório: Nenhum resultado
-                            </h5>
+                                <li className={styles.completed}> 
+                                    <h4><FiUserPlus /> Completos</h4>
+                                    <h4>1</h4>
+                                </li>
 
-                        ) : (
-                                challengesCompleted > challengesFailed ? (
-                                    <h5>
-                                        <FiTrendingUp color={"#5965e0"} />
-                                        Relatório: Seus resultados estão no padrão esperado
-                                    </h5>
-                                ) :
-                                    <h5>
-                                        <FiTrendingDown color={"#e03a86"} />
-                                        Relatório: Seus resultados estão abaixo do esperado
-                                    </h5>
-                            )
-                        }
-                    </div>
-                </section>
-                </details>
+                                <li className={styles.failed}>   
+                                    <h4><FiUserMinus /> Incompletos</h4>
+                                    <h4>1</h4>
+                                </li>
+
+                                <li className={styles.level}>  
+                                    <h4><FiArrowUp /> Level</h4>
+                                    <h4>1</h4>
+                                </li>
+                            </ul>
+                        </div>
+                    </section>               
+                </div>
             </div>
-       
+        </>
     );
 
 
