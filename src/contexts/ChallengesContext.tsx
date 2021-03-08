42ea4c7ee0b10e75ea0e74269717e115a2613346
Your Name <charlesmartins.ads@gmail.com>
2021-03-08 18:59:57 -0300

    fixed start level

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -45,7 +45,7 @@ export const ChallengesContext = createContext({} as ChallengesContextData)
 
 export function ChallengesProvider({ children, ...rest }: ChallengesProviderProps) {
 
-   const [level, setLevel] = useState(rest.level ?? 0);
+   const [level, setLevel] = useState(rest.level ?? 1);
    const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
    const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
    const [challengesFailed, setChallengesFailed] = useState(rest.challengesFailed ?? 0);   
