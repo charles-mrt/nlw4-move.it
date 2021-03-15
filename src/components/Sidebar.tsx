01c6c53e430c432b204fe6adb05ef676176989df
Your Name <charlesmartins.ads@gmail.com>
2021-03-15 10:02:54 -0300

    added link target

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,7 +6,8 @@ import Link from 'next/link';
 
 
 export function Sidebar() {
- 
+
+  
    return(
       <>
          <aside className={styles.appSidebar}>
@@ -14,9 +15,9 @@ export function Sidebar() {
                <ul>                  
                   { SidebarData.map(( item, index ) => {
                      return(
-                        <li key={index} className={item.cName} >
+                        <li key={index} className={item.cName}>
                            <Link href={item.path}>                                 
-                              <a title={item.title}> {item.icon}</a>                                 
+                              <a title={item.title} target={item.target}> {item.icon}</a>                                 
                            </Link>
                         </li>
                         )
