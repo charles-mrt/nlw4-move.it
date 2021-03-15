924c31b6ff1e05e81f721e1f7f562d01e71118e3
Your Name <charlesmartins.ads@gmail.com>
2021-03-15 10:08:26 -0300

    changed user avatar

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,20 +2,24 @@ import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
-const avatarUrl = "https://avatars.githubusercontent.com/u/60299718?s=400&u=435f6098dd446f0426b498c26a1bfe335c83abf9&v=4"
-let userAvatar = avatarUrl ? avatarUrl : "/assets/userAvatar.svg";  ;
+
+
+const avatarUrl = "/assets/userAvatar.svg";
+
+
 
 export function Profile() {
+    
+    const {level, userName} = useContext(ChallengesContext)      
 
-    const {level} = useContext(ChallengesContext)
+   
 
 
     return (
         <div className={ styles.profileContainer }>
-            <img src={userAvatar} alt="Charles Martins"/>
-            
+            <img src={avatarUrl} alt={userName}/>
             <div>
-                <strong>Charles Martins</strong>
+                <strong>nome: {userName} </strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
                     Level {level}
