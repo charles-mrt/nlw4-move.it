d93ddfa9f34012843a9e93c04bd5b6da3f27fb02
Your Name <charlesmartins.ads@gmail.com>
2021-03-15 11:05:20 -0300

    removed white spaces on code

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,19 +2,11 @@ import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
-
-
 const avatarUrl = "/assets/userAvatar.svg";
-
-
 
 export function Profile() {
     
     const {level, userName} = useContext(ChallengesContext)      
-
-   
-
-
     return (
         <div className={ styles.profileContainer }>
             <img src={avatarUrl} alt={userName}/>
