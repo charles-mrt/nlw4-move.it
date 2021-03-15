e0b5433df163cdf9469e39f9e5a1610149145389
Your Name <charlesmartins.ads@gmail.com>
2021-03-15 11:42:49 -0300

    added new props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,31 +4,46 @@ import { ChallengesProvider } from '../contexts/ChallengesContext';
 import { Login } from "../components/Login";
 
 interface HomeProps {
-   userName: string, 
- }
- export default function userLogin(props) {
-   
-   return (
-      <ChallengesProvider
-         userName={props.userName}
-      >         
-       <div>
+  userName: String,
+  level: number,
+  currentExperience: number,
+  challengesCompleted: number,
+  challengesFailed: number,
+  totalChallenge: number
+
+}
+export default function userLogin(props) {
+
+  return (
+    <ChallengesProvider
+      userName={props.userName}
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+      challengesFailed={props.challengesFailed}
+      totalChallenge={props.totalChallenge}
+    >
+      <div>
         <Head> <title> Login | move.it </title></Head>
-          <Login />
-       </div>
-      </ChallengesProvider>
-   )
+        <Login />
+      </div>
+    </ChallengesProvider>
+  )
 }
 
 /* get cookies to be used in the component */
-
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
-   const { userName} = ctx.req.cookies
- 
-   return {
-     props: {
-       userName: String(userName),
-     }
-   }
- }
+  const { userName, level, currentExperience, challengesCompleted, challengesFailed } = ctx.req.cookies
+
+  return {
+    props: {
+      userName: String(userName),
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+      challengesFailed: Number(challengesFailed),
+      totalChallenge: Number(challengesFailed)
+    }
+  }
+}
