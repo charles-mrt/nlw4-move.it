3237fbc9bac46f5b1a963cb66daf0bb1bf4f363b
Your Name <charlesmartins.ads@gmail.com>
2021-03-19 10:14:27 -0300

    removed white space in code

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,30 +1,27 @@
 import styles from '../styles/components/Sidebar.module.css';
 
-import {SidebarData} from '../utils/SidebarData';
+import { SidebarData } from '../utils/SidebarData';
 
 import Link from 'next/link';
 
 
 export function Sidebar() {
 
-  
-   return(
-      <>
+return(      
          <aside className={styles.appSidebar}>
             <nav>
                <ul>                  
                   { SidebarData.map(( item, index ) => {
                      return(
-                        <li key={index} className={item.cName}>
-                           <Link href={item.path}>                                 
-                              <a title={item.title} target={item.target}> {item.icon}</a>                                 
+                        <li key={index} className={item.cName} >
+                           <Link href={item.path}>                              
+                              <a title={item.title} target={item.target} >{item.icon}</a>                                 
                            </Link>
                         </li>
-                        )
-                     })}                 
+                     )
+                  })}                 
                </ul>
             </nav>
-         </aside>
-      </>
-   );
+         </aside>      
+      );
 }
