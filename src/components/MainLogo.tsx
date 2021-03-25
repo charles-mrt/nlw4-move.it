e308a89631ceca3837f029f2d5dcb3ad56535ad7
Your Name <charlesmartins.ads@gmail.com>
2021-03-25 08:50:23 -0300

    added version

--- src/components/MainLogo.tsx
+++ src/components/MainLogo.tsx
@@ -1,13 +1,11 @@
 import styles from '../styles/components/MainLogo.module.css';
 
 export function MainLogo() {
-   
+
     return (
-        <>             
-            <div className={ styles.mainLogo }>            
-                <img src="/assets/logo.svg" alt="MoveIT" className={styles.logo}/>
-               {<small>Em desenvolvimento - time teste 0:3s</small>}
-            </div>
-        </>
+        <div className={styles.mainLogo}>
+            <img src="/assets/logo.svg" alt="MoveIT" className={styles.logo} />
+            {<small>v.1.0.1</small>}
+        </div>
     );
 }
