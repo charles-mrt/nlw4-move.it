847689dd95dff8b7403bccbf4db2d9d8a0d2475c
Your Name <charlesmartins.ads@gmail.com>
2021-03-25 08:57:07 -0300

    added switch to select demo time

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,29 +1,52 @@
-import { useState, useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import { CountDownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/Countdown.module.css';
-import { FiChevronsRight, FiXCircle, FiCheck} from "react-icons/fi";
+import { FiChevronsRight, FiXCircle, FiCheck, FiClock } from "react-icons/fi";
 
+import Switch from 'react-switch';
 
 export function Countdown() {
-    
-    const {      
-        minutes, 
-        seconds, 
-        hasFinished, 
-        isActive, 
-        startCountdown, 
+
+    const {       
+        demoTime,
+        startTime,
+        checked,
+        lockToggle,
+        minutes,
+        seconds,
+        hasFinished,
+        isActive,
+        handleChange,
+        startCountdown,
         resetCountdown,
     } = useContext(CountDownContext);
 
     /* split value to add in array['',''] if result is not decimal add 0 before to split */
     const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
-
+    
     return (
-        <div>
-            <div>                                             
-            </div>
-           
+        <div className={styles.countdown}>
+            <header>
+                {!checked ? (
+                    <p><FiClock /> ativar tempo de teste?</p>
+                ) :
+                    <p><FiClock color={"#5965e0"} /> tempo de teste ativado!</p>}
+
+                {<Switch
+                    onChange={handleChange}
+                    checked={checked}
+                    checkedIcon={false}
+                    uncheckedIcon={false}
+                    height={10}
+                    width={40}
+                    handleDiameter={18}
+                    onColor={"#5965e0"}
+                    offColor={"#2e384d"}
+                    disabled={lockToggle}
+                />}
+            </header>
+
             <div className={styles.countdownContainer}>
 
                 <div>
@@ -42,36 +65,47 @@ export function Countdown() {
 
             {hasFinished ? (
                 <button
-                 disabled
-                 className={styles.countdownButton}
+                    disabled
+                    className={styles.countdownButton}
                 >
-                    Ciclo encerrado <FiCheck size={"2rem"} color={"#2ecc71"}/>
+                    Ciclo encerrado <FiCheck size={"2rem"} color={"#2ecc71"} />
                 </button>
             ) : (
                     <>
-                      { isActive ? (
-                      
-                        <button
-                         type="button"
-                         className={`${styles.countdownButton} ${styles.countdownButtonActive} `}
-                         onClick={resetCountdown}                                                                          
-                                                  
-                        >
-                         Abandonar ciclo  <FiXCircle size={"2rem"}/>
-                          <span className={`${styles.countdownButtonLoadBar}`} />
-                        </button>                        
-                      ) : ( 
-                        <button
-                         type="button"
-                         className={styles.countdownButton}
-                         onClick={startCountdown}
-                        >
-                         Iniciar um ciclo <FiChevronsRight size={"2rem"}/> 
-                        </button>
-                      )}
+                        { isActive ? (
+                            <button
+                                type="button"
+                                className={`${styles.countdownButton} ${styles.countdownButtonActive} `}
+                                onClick={resetCountdown}
+                            >
+                                Abandonar ciclo  <FiXCircle size={"2rem"} />
+                                <span>
+                                    <style jsx> 
+                                        {
+                                           `content: "";
+                                            position: absolute;
+                                            height: 0.2rem;
+                                            left: 0;
+                                            bottom: 0;
+                                            background: var(--green);
+                                            animation: loadBar ${checked ? demoTime : startTime }s linear normal;
+                                            
+                                            @keyframes loadBar { 0% {width: 0%;} 100% {width: 100%;}}`
+                                        }
+                                    </style>
+                                </span>
+                            </button>
+                        ) : (
+                                <button
+                                    type="button"
+                                    className={styles.countdownButton}
+                                    onClick={startCountdown}
+                                >
+                                    Iniciar um ciclo <FiChevronsRight size={"2rem"} />
+                                </button>
+                            )}
                     </>
                 )}
         </div>
     );
-
 }
