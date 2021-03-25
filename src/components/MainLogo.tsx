dcb8de5d87a1eb78e9836295621a66372c45bf70
Your Name <charlesmartins.ads@gmail.com>
2021-03-25 08:54:18 -0300

    added version

--- src/components/MainLogo.tsx
+++ src/components/MainLogo.tsx
@@ -5,7 +5,7 @@ export function MainLogo() {
     return (
         <div className={styles.mainLogo}>
             <img src="/assets/logo.svg" alt="MoveIT" className={styles.logo} />
-            {<small>v.1.0.1</small>}
+            <small>v.1.0.1</small>
         </div>
     );
 }
